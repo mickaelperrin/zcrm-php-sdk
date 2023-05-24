4cda26364e6c017b721d97e01564c0d4e60bd1e5
Mickaël Perrin <contact@mickaelperrin.fr>
2023-05-24 11:09:45 +0200

    [BUGFIX] Wrong types
    Type of zcrmsdk\oauth\exception\ZohoOAuthException::$file must be string (as in class Exception)

--- src/oauth/exception/ZohoOAuthException.php
+++ src/oauth/exception/ZohoOAuthException.php
@@ -11,13 +11,7 @@ class ZohoOAuthException extends \Exception
     
     // Unknown
     protected $code = 0;
-    
-    // User-defined exception code
-    protected $file;
-    
-    // Source filename of exception
-    protected $line;
-    
+
     // Source line of exception
     private $trace;
     
